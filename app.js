ec9f2a5096f022d5172ee6f3cb1b1a6486c30382
caner <canerorenn@gmail.com>
2025-02-18 17:27:57 +0300

    Installed ejs createad middlewares and views-partials

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const express = require("express");
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
-app.get('/',(req,res)=>{
-  const blog = {id:1,title:"Blog title",description:"Blog description"};
-  res.send(blog);
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLWARES
+app.use(express.static('public'));
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-const port =3000;
-app.listen(port, ()=>{
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post',(req,res)=>{
+  res.render('post');
+});
+
+const port = 3001;
+app.listen(port, () => {
   console.log(`Server initialized at ${port} port`);
 });
