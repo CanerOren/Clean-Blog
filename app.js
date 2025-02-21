3442948dbffb58cd52bb2028d8ab39abd1aef7cb
caner <canerorenn@gmail.com>
2025-02-21 22:45:12 +0300

    Post.js model created and dynamic site created with post method

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-const express = require('express');
-const ejs = require('ejs');
-const path = require('path');
+import express from 'express';
+// const express = require('express');
+import ejs from 'ejs'
+// const ejs = require('ejs');
+import mongoose from 'mongoose';
+
+import path from 'path';
+// const path = require('path');
+
+import Post from './models/Post.js';
 
 const app = express();
+
+// connect DB
+mongoose.connect('mongodb://localhost/clean-blog-db')
+  .then(()=>{console.log('Succesfully connected MongoDB')})
+  .catch((err)=>{console.log(`Connection error : ${err}`)});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index',{
+    posts
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -26,6 +43,11 @@ app.get('/add_post', (req, res) => {
 app.get('/post',(req,res)=>{
   res.render('post');
 });
+
+app.post('/posts',async(req,res)=>{
+  await Post.create(req.body);
+  res.redirect('/');
+})
 
 const port = 3001;
 app.listen(port, () => {
